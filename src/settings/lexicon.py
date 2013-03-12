14d91aa72f9b520e9d76eaf938edc2eed3dc8ff8
Jacky Tian <xjtian94@gmail.com>
2013-03-12 16:26:51 -0400

    Proper relative paths. Remember to set working directories in run configurations to top-level.

--- src/settings/lexicon.py
+++ src/settings/lexicon.py
@@ -1,6 +1,6 @@
 __author__ = 'Jacky'
 
-WORDLIST_PATH = './../../wordlists/OSPD4_stripped.txt'
+WORDLIST_PATH = 'wordlists/OSPD4_stripped.txt'
 WORDLIST_NAME = 'Official Scrabble Players Dictionary 4'
 
-GADDAG_PICKLE_PATH = './../../pickles/OSPD4_gaddag.pkl'
+GADDAG_PICKLE_PATH = 'pickles/OSPD4_gaddag.pkl'
