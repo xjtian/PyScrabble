e0e29674597be97d802957164e14785b138b5c51
Jacky Tian <xjtian94@gmail.com>
2013-06-06 19:14:22 -0400

    Finished migration to scenario-based tests.

--- engine/test/test_game.py
+++ engine/test/test_game.py
@@ -90,132 +90,24 @@ class TestScrabbleGame(unittest.TestCase):
         self.assert_(not self.game.set_candidate('', (7, 7), True))
 
     def test_validate_candidate(self):
-        self.game.add_player('Bob')
-        self.game.players[0].rack = ['H', 'E', 'L', 'L', 'O', ' ', ' ']
-
-        hello = 'HELLO'
-
         def clear_game():
             self.game.candidate = None
             self.game.board = deepcopy(board.default_board)
 
         # Drop-in replacement for testing openings using scenario tester
         self.__scenario_tester('./engine/test/scenarios/opening.txt')
-
         clear_game()
 
         # Drop-in replacement for testing crosses using scenarios
         self.__scenario_tester('./engine/test/scenarios/crosses.txt')
-
         clear_game()
 
-        # Now try a parallel move
-        self.game.history = None
-
-        self.game.set_candidate(hello, (7, 7), False)
-        self.game.validate_candidate()
-
-        self.game.history = object()
-        self.game.candidate = None
-        self.game.set_candidate('EL', (7, 8), False)    # Forms EL, HE, and EL
-        self.assert_(self.game.validate_candidate())
-        self.assertEqual(11, self.game.candidate.score)
-
+        # Testing parallel moves
+        self.__scenario_tester('./engine/test/scenarios/parallel.txt')
         clear_game()
-        self.game.history = None
 
-        self.game.set_candidate('AB', (7, 7), True)
-        self.assert_(self.game.validate_candidate())
-        self.assertEqual(0, self.game.candidate.score)
-
-        # Try a move with a tile on the edge
-        clear_game()
-        self.game.history = None
-
-        self.game.set_candidate(hello, (3, 7), False)
-        self.game.validate_candidate()
-
-        self.game.history = object()
-        self.game.candidate = None
-        self.game.set_candidate('ELLO', (3, 8), True)
-        self.game.validate_candidate()
-
-        self.game.candidate = None
-        self.game.set_candidate('LW', (2, 11), False)
-        self.assert_(self.game.validate_candidate())
-
-        self.game.candidate = None
-        self.game.set_candidate('HAT', (4, 12), True)
-        self.assert_(self.game.validate_candidate())
-
-        # Try an invalid first move
-        clear_game()
-        self.game.history = None
-        self.game.set_candidate(hello, (8, 8), True)
-        self.assert_(not self.game.validate_candidate())
-
-        # Try some invalid crosses now
-        clear_game()
-        self.game.set_candidate(hello, (7, 7), False)
-        self.game.validate_candidate()
-
-        self.game.history = object()
-        self.game.candidate = None
-        self.game.set_candidate(hello, (9, 9), True)     # No hook or cross
-        self.assert_(not self.game.validate_candidate())
-
-        clear_game()
-        self.game.set_candidate(hello, (7, 7), False)
-        self.game.validate_candidate()
-
-        self.game.history = object()
-        self.game.candidate = None
-        self.game.set_candidate('HO', (7, 8), True)     # Not a word (HHO)
-        self.assert_(not self.game.validate_candidate())
-
-    def test_specific_cases(self):
-        """
-        Some tests for unexpected outcomes encountered when playing the game
-        """
-
-        def parse_situation(s):
-            lines = situation.splitlines()
-            return [line[line.index('|') + 2:].split(' ') for line in lines]
-
-        # The problem with this case was that the prefix finder wrapped
-        # around because the first letter was on an edge. 'HUMVEE' was added
-        # as a prefix to the attempted play 'RASING', which clearly made it
-        # invalid
-
-        situation = """ 0| # . . 2 . . . J I L T 2 . . #
-             1| . @ . . . Y . . . 3 O W . @ .
-             2| . . @ . . O F . 2 . K I P . .
-             3| 2 . . @ . D I F . D E N E . 2
-             4| . . . . @ . Z A . I D . A . .
-             5| . 3 . . . 3 . C . V . . R 3 .
-             6| . . 2 . . . 2 a W A . . L . G
-             7| # . . 2 . . . D O N . C . . R
-             8| . . 2 . . . Y E N . . O 2 . I
-             9| . 3 . . . 3 O . . 3 H E . U M
-            10| . . . . P L U G . . A N . R E
-            11| 2 . . Q I . . R . . B A . T 2
-            12| . . @ . . . 2 I 2 H U M V E E
-            13| . @ . . . 3 . O . 3 . O . X .
-            14| # . . 2 . . . T o L E R A T E"""
-        current_rack = list('RAASIGN')
-        attempt = {'letters': 'RASING', 'pos': (12, 0), 'horiz': True}
-
-        # Setup the game
-        self.game.add_player('Player 1')
-        self.game.add_player('Player 2')
-        self.game.current_turn = 1
-        self.game.players[1].rack = current_rack
-        self.game.history = object()
-
-        self.game.board = parse_situation(situation)    # Set the board
-        # Set the candidate attempt to the improper failure
-        self.assert_(self.game.set_candidate(attempt['letters'], attempt['pos'], attempt['horiz']))
-        self.assert_(self.game.validate_candidate())
+        # Testing specific scenarios that came up in playtesting
+        self.__scenario_tester('./engine/test/scenarios/specific.txt', False)
 
     def test_remove_candidate(self):
         self.game.add_player('Bob')
@@ -297,9 +189,11 @@ class TestScrabbleGame(unittest.TestCase):
         # Make sure that exchange works properly after a failed attempt
         self.assert_(self.game.exchange_tiles('AB'))
 
-    def __scenario_tester(self, filename):
+    def __scenario_tester(self, filename, wordlist=True):
         old_set = self.game.lexicon_set
-        self.game.lexicon_set = lexicon_set.read_lexicon('./engine/test/wordlists/wordlist1.txt')
+        if wordlist:
+            self.game.lexicon_set = lexicon_set.read_lexicon('./engine/test/wordlists/wordlist1.txt')
+
         for exp_results in parse_scenario(filename):
             self.game.candidate = None
             self.game.board = exp_results['board']
