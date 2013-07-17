ed8a937dfe37931ee0106ee4729747a8651c008e
Jacky Tian <xjtian94@gmail.com>
2013-07-16 20:09:54 -0400

    Parallel mid set test cases up to suffix

--- strategy/test/test_cross_sets.py
+++ strategy/test/test_cross_sets.py
@@ -18,6 +18,7 @@ class TestCrossSets(unittest.TestCase):
         self.__scenario_tester('./strategy/test/scenarios/mids.txt')
         self.__scenario_tester('./strategy/test/scenarios/specific.txt')
         self.__scenario_tester('./strategy/test/scenarios/parallel_presuff.txt')
+        self.__scenario_tester('./strategy/test/scenarios/parallel_mids.txt')
 
     def __scenario_tester(self, filename):
         for scenario in parse_cross_set(filename):
